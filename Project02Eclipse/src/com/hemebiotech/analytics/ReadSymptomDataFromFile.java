6a986d9fe594cef76a23631dbce0b010ff0fabc8
Pierrick Levazeux <pierrick.levazeux@protonmail.com>
2022-01-11 22:44:15 +0100

    Add documentation for class ReadSymptomDataFromFile

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -7,25 +7,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Simple brute force implementation
- *
+ * ReadSymptomDataFromFile class read symptoms list from a text file
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
 	
 	/**
-	 * 
+	 * ReadSymptomDataFromFile
+	 *
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
 	 */
 	public ReadSymptomDataFromFile (String filepath) {
 		this.filepath = filepath;
 	}
-	
+
+	/**
+	 * The GetSymptoms method reads line by line a text file that contains symptom strings (one per line)
+	 *
+	 * @return a list of symptom strings that may contain many duplications. If no data in text file, returns an empty list.
+	 */
 	@Override
 	public List<String> GetSymptoms() {
 		ArrayList<String> result = new ArrayList<String>();
-		
+
 		if (filepath != null) {
 			try {
 				BufferedReader reader = new BufferedReader (new FileReader(filepath));
@@ -40,8 +45,6 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 				e.printStackTrace();
 			}
 		}
-		
 		return result;
 	}
-
 }
